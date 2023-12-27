d198cbe60c6d9f339430272041cd0c35281dd994
rjunio98 <robledorjunior1@gmail.com>
2023-12-27 20:57:05 -0300

    Add PlaceItemCard component to PlaceList

--- app/components/PlaceList.tsx
+++ app/components/PlaceList.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import PlaceItemCard from "./PlaceItemCard";
 
-function PlaceList({placeList}:any) {
+function PlaceList({ placeList }: any) {
   return (
     <div className="px-[10px] md:px-[120px] mt-7">
       <h2 className="text-[20px] font-bold">Search Results</h2>
+      <div>
+        {placeList.map((place: any, index: number) => (
+          <div>
+            <PlaceItemCard place={place}/>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
