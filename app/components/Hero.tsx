8f1481c844d02814d04d5a9f004afa40250f6534
rjunio98 <robledorjunior1@gmail.com>
2023-12-27 13:40:00 -0300

    Add category browsing feature to Hero component

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import category from "../data/category";
 
 function Hero() {
   return (
@@ -23,7 +24,7 @@ function Hero() {
             <input
               type="text"
               placeholder="Search Anything"
-              className=" bg-white z-10 p-3 border-[1px] rounded-full px-5 w-[36%] shadow-sm"
+              className=" bg-white z-10 p-3 border-[1px] rounded-full px-5 w-[36%] shadow-sm outline-red-300"
             />
             <button className="bg-red-600 rounded-full p-3 shadow-md z-10 cursor-pointer hover:scale-105 transition-all">
               <svg
@@ -41,6 +42,22 @@ function Hero() {
                 />
               </svg>
             </button>
+          </div>
+          <div className="mt-5 flex flex-col justify-center items-center">
+            <h2>Or Browse the category</h2>
+
+            <div className="grid grid-cols-3 md:grid-cols-7 w-[50%] justify-center gap-5 mt-3">
+              {category.map((item, index) => (
+                <div key={index} className="border-[1px] w-[60px] p-4 bg-white rounded-full z-10 hover:border-red-600 hover:scale-110 cursor-pointer transition-all">
+                  <Image
+                    src={item.icon}
+                    alt={item.name}
+                    width={30}
+                    height={30}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
