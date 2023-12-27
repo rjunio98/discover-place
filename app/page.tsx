d61486581c8b601829e527764074743ef5c711ce
rjunio98 <robledorjunior1@gmail.com>
2023-12-27 19:29:40 -0300

    Add dynamic fetching of place list from Google Places API

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import Image from "next/image";
 import Hero from "./components/Hero";
 import PlaceList from "./components/PlaceList";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [placeList, setPlaceList] = useState([]);
+
+  useEffect(() => {
+    getPlaceList();
+  }, []);
+
+  const getPlaceList = async () => {
+    const result = await fetch("/api/google-place-api?q=Hotels In NewYork");
+    const data = await result.json();
+
+    setPlaceList(data.resp.results);
+  };
+
   return (
     <div>
       <Hero />
-      
-      <PlaceList />
+
+      {placeList? <PlaceList placeList={placeList}/>:null}
     </div>
   );
 }
