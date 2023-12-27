287f93c7bf8b8ca55fbf04ded1dded2b38444494
rjunio98 <robledorjunior1@gmail.com>
2023-12-27 16:23:02 -0300

    Add PlaceList component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import PlaceList from "./components/PlaceList";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      
+      <PlaceList />
     </div>
   );
 }
