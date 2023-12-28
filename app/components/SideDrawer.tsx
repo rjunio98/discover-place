d88d4ed98276f7b2867e56428f08809983325708
rjunio98 <robledorjunior1@gmail.com>
2023-12-28 00:13:55 -0300

    Add SideDrawer component

---
+++ app/components/SideDrawer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SideDrawer() {
+  return (
+    <div className="h-screen w-screen md:w-[400px] bg-white shadow-md p-5 z-20">SideDrawer</div>
+  )
+}
+
+export default SideDrawer
